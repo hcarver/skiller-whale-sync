956ea8ba1a61526130ea67538803cfe13963b8fc
Hywel Carver <>
2018-12-12 20:34:29 +0000

    v1

---
+++ content-watcher.js
@@ -0,0 +1,78 @@
+const path = require("path")
+const fs = require("fs")
+const crypto = require("crypto")
+const http = require("http")
+const process = require("process")
+
+let fileHashes = new Map()
+const IGNORE_DIRS = ["node_modules", ".git"]
+const WATCHED_EXTS = [".jsx", ".js"]
+
+function putUpdate(path) {
+  const data = JSON.stringify({
+    relative_path: path,
+    contents: fs.readFileSync(path).toString()
+  })
+
+  const options = {
+    hostname: process.env.SERVER_URL || "train.skillerwhale.com",
+    port: process.env.SERVER_PORT || "80",
+    path: `/attendances/${process.env.ATTENDANCE_ID}/file_snapshots`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length
+    }
+  }
+
+  const req = http.request(options, res => {
+    console.log(`status: ${res.statusCode}`)
+
+    res.on("data", d => {
+      process.stdout.write(d)
+    })
+  })
+
+  req.on("error", error => {
+    console.error(error)
+  })
+  req.write(data)
+  req.end()
+}
+
+function hashFile(path) {
+  const hasher = crypto.createHash("sha256")
+  hasher.update(fs.readFileSync(path))
+  return hasher.digest("hex")
+}
+
+const pollDirectoryForChanges = dirPath => {
+  if (IGNORE_DIRS.includes(path.basename(dirPath))) return
+  const files = fs.readdirSync(dirPath)
+  files.forEach(file => {
+    const newPath = path.join(dirPath, file)
+    const stats = fs.statSync(newPath)
+    if (stats.isDirectory()) {
+      pollDirectoryForChanges(newPath)
+    } else {
+      if (WATCHED_EXTS.includes(path.extname(newPath))) {
+        const currentHash = hashFile(newPath)
+        if (fileHashes.has(newPath)) {
+          const oldHash = fileHashes.get(newPath)
+          if (oldHash !== currentHash) {
+            console.log(`file changed ${newPath}`)
+            putUpdate(newPath)
+          }
+        }
+        fileHashes.set(newPath, currentHash)
+      }
+    }
+  })
+}
+
+const pollerFunction = () => {
+  pollDirectoryForChanges(".")
+  setTimeout(pollerFunction, 1000)
+}
+
+pollerFunction()
---
+++ index.js
@@ -0,0 +1,43 @@
+const readline = require("readline")
+const spawn = require("cross-spawn")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+module.exports = function swsync (other_cmd) {
+  cl = console.log
+  cl("  _____ _    _ _ _            __          ___           _      ")
+  cl(" / ____| |  (_) | |           \\ \\        / / |         | |     ")
+  cl("| (___ | | ___| | | ___ _ __   \\ \\  /\\  / /| |__   __ _| | ___ ")
+  cl(" \\___ \\| |/ / | | |/ _ \\ '__|   \\ \\/  \\/ / | '_ \\ / _` | |/ _ \\")
+  cl(" ____) |   <| | | |  __/ |       \\  /\\  /  | | | | (_| | |  __/")
+  cl("|_____/|_|\\_\\_|_|_|\\___|_|        \\/  \\/   |_| |_|\\__,_|_|\\___| ")
+  cl("")
+  console.log(
+    "Please copy and paste your ID from the course page here and press enter.\n"
+  )
+  rl.question("Trainee ID: ", attendanceId => {
+    console.log("")
+    console.log("Great! We're going to start a development server running now")
+    console.log("and we'll be watching your directory here for changes so that")
+    console.log("the trainer can see your progress. Hit Ctrl+C twice to stop.")
+    console.log("")
+    const server = spawn.spawn(
+      "npx",
+      ["concurrently", '"' + other_cmd + '"', '"content-sync"'],
+      { env: { ...process.env, ATTENDANCE_ID: attendanceId.trim() } }
+    )
+
+    server.on("error", data => {
+      console.log(`error: ${data}`)
+    })
+    server.stdout.on("data", data => {
+      process.stdout.write(data)
+    })
+    server.stderr.on("data", data => {
+      process.stderr.write(data)
+    })
+  })
+}
